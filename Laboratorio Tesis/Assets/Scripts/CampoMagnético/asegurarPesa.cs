4c38be1c0f18ba03185849657915c2d09d9c9bb0
daecheverria <decheverria@correo.unimet.edu.ve>
2025-10-27 16:32:10 -0400

    Prueba

--- Laboratorio Tesis/Assets/Scripts/CampoMagnético/asegurarPesa.cs
+++ Laboratorio Tesis/Assets/Scripts/CampoMagnético/asegurarPesa.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
 public class asegurarPesa : MonoBehaviour
 {
     private Collider colPesa;
     public Collider colResorte;
     public ManejadorPesas manejadorPesas; // Referencia al manejador
+    public XRSocketInteractor socket;
 
     public void Asegurar(Collider colPesa)
     {
@@ -17,6 +19,14 @@ public class asegurarPesa : MonoBehaviour
         colPesa = manejadorPesas.pesaCol;
         colPesa.enabled = true;
         colResorte.enabled = false;
+        StartCoroutine(TemporarilyDisableSocket(0.2f));
+        
     }
-
+    private IEnumerator TemporarilyDisableSocket(float delay)
+    {
+        // Desactiva el componente para evitar que vuelva a enganchar inmediatamente
+        socket.enabled = false;
+        yield return new WaitForSeconds(delay);
+        socket.enabled = true;
+    }
 }
